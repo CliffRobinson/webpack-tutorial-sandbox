d6cb71cd357416304a5009071a77c70b509a321f
CliffRobinson <37293617+CliffRobinson@users.noreply.github.com>
2020-02-07 15:42:27 +1300

    add xml data

--- src/LodashDomScript.js
+++ src/LodashDomScript.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import "./style.css";
 import galliaImg from "./images/Gallia-of-the-Endless-Dance-Theros-Beyond-Death-Art.jpg";
+import data from "./data/data.xml";
 
 export function addLodashEl() {
   const element = document.createElement("div");
@@ -9,7 +10,7 @@ export function addLodashEl() {
     ["whaddup", "chumps", "we", "packin", "webs", "nauuuu"],
     "---"
   );
-
+  console.log(data);
   return element;
 }
 
