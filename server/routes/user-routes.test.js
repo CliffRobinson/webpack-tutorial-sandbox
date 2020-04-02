67905b36fd6151a40b9d5e542ee5e237c381a413
CliffRobinson <clifford.f.robinson@gmail.com>
2020-04-02 17:23:10 +1300

    abandon server error test

--- server/routes/user-routes.test.js
+++ server/routes/user-routes.test.js
@@ -10,14 +10,4 @@ describe('/routes/user-routes', () => {
             .expect('Content-Type', /json/)
             .expect(200, done)
     })
-
-    it('throws errors', ()=> {
-        const text = "this is an error message"
-        const scope = nock("localhost:3000")
-            .get('/users')
-            .replyWithError(text)
-
-        return supertest(server)
-            .get('/users')
-    })
 })
