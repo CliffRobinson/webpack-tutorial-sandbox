88c2fa3ad202d18aacd7d67e3fe84f49cb6b7dbb
CliffRobinson <clifford.f.robinson@gmail.com>
2020-05-14 20:00:20 +1200

    finish socket game functions

--- server/socket/games.js
+++ server/socket/games.js
@@ -1,6 +1,6 @@
 const log = require('loglevel')
 
-const { RECEIVE_GAMES_BY_STATUS, REQUEST_GAMES_BY_STATUS, CREATE_GAME } = require('../../constants/events')
+const { RECEIVE_GAMES_BY_STATUS, REQUEST_GAMES_BY_STATUS, CREATE_GAME, UPDATE_GAME, DELETE_GAME } = require('../../constants/events')
 const gamesDb = require('../db/games')
 
 module.exports = (socket, serverSocket) => {
@@ -24,5 +24,31 @@ module.exports = (socket, serverSocket) => {
                         serverSocket.emit('dispatch', {dispatchFunction: "receiveGamesByStatus", payload: {status: "pending", games}})
                     })
             })
+    })
+
+    socket.on(UPDATE_GAME, (game) => {
+        log.trace(`socket is updating game ${game.id} in db`)
+        gamesDb.updateGame(game)
+            .then((result) => {
+                log.trace(`result of update is ${result}`)
+                gamesDb.getGamesByStatus(game.status)
+                    .then(games => {
+                        log.trace(`just did getGamesByStatus(${game.status}) after updateGame, emitting an array of ${games.length}`)
+                        serverSocket.emit('dispatch', {dispatchFunction: "receiveGamesByStatus", payload: {status: game.status, games}})
+                    })
+            })
+    })
+
+    socket.on(DELETE_GAME, (id) => {
+        log.trace(`socket is deleting game id:${id} in db`)
+        gamesDb.deleteGame(id)
+            .then((result) => {
+                log.trace(`result of game delete is ${result}`)
+                gamesDb.getGamesByStatus("pending")
+                    .then(games => {
+                        log.trace(`just did getGamesByStatus(pending) after createGame, emitting an array of ${games.length}`)
+                        serverSocket.emit('dispatch', {dispatchFunction: "receiveGamesByStatus", payload: {status: "pending", games}})
+                    })
+            }) 
     })
 }
--- server/socket/socket-server-games.test.js
+++ server/socket/socket-server-games.test.js
@@ -2,18 +2,22 @@ const mockGames = require('../db/seeds/test/test-games').data
 
 //const {getGamesByStatus, createGame} = require('../db/games')
 
-const mockGetGamesByStatus = jest.fn((status) => new Promise( (res, rej) => res((mockGames.filter((game) => (game.status == status))))))
-const mockCreateGame = jest.fn((game) => new Promise( (res, rej) => res(5) ))
+const mockGetGamesByStatus = jest.fn((status) => new Promise((res, rej) => res((mockGames.filter((game) => (game.status == status))))))
+const mockCreateGame = jest.fn((game) => new Promise((res, rej) => res([5])))
+const mockUpdateGame = jest.fn((game) => new Promise((res, rej) => res(game.id)))
+const mockDeleteGame = jest.fn((game) => new Promise((res, rej) => 1))
 
 jest.mock('../db/games', () => ({
   getGamesByStatus: mockGetGamesByStatus,
-  createGame: mockCreateGame
+  createGame: mockCreateGame,
+  updateGame: mockUpdateGame,
+  deleteGame: mockDeleteGame
 }))
 
 const io = require('socket.io-client')
 const http = require('http')
 
-const { REQUEST_GAMES_BY_STATUS, RECEIVE_GAMES_BY_STATUS, CREATE_GAME } = require('../../constants/events')
+const { REQUEST_GAMES_BY_STATUS, RECEIVE_GAMES_BY_STATUS, CREATE_GAME, UPDATE_GAME, DELETE_GAME } = require('../../constants/events')
 
 // Test boilerplate stolen from: https://medium.com/@tozwierz/testing-socket-io-with-jest-on-backend-node-js-f71f7ec7010f
 // Tomasz Zwierzchoń, you're a prince among men.
@@ -79,26 +83,57 @@ afterEach((done) => {
 
 describe('server socket game functions', () => {
 
-    test('socket gets games by status from db and emits receive games', (done) => {
-        const status = "pending"
-        socket.emit(REQUEST_GAMES_BY_STATUS, status)
-        setTimeout( () => {
-            expect(spy).toHaveBeenCalledWith('dispatch', {
-              dispatchFunction:'receiveGamesByStatus', payload: {status, games: mockGames.slice(0,2)}
-            })
-            done()
-        }, 50)
-    })
+  test('socket gets games by status from db and emits receive games', (done) => {
+    const status = "pending"
+    socket.emit(REQUEST_GAMES_BY_STATUS, status)
+    setTimeout(() => {
+      expect(spy).toHaveBeenCalledWith('dispatch', {
+        dispatchFunction: 'receiveGamesByStatus', payload: { status, games: mockGames.slice(0, 2) }
+      })
+      done()
+    }, 50)
+  })
 
-    test('socket calls db.createGame, then getGames(pending), then emit dispatch to client socket', (done) => {
-      socket.emit(CREATE_GAME, mockGames[0])
+  test('socket calls db.createGame, then getGames(pending), then emit dispatch to client socket', (done) => {
+    socket.emit(CREATE_GAME, mockGames[0])
 
-      setTimeout( () => {
-        expect(mockCreateGame.mock.calls[0][0]).toEqual(mockGames[0])
-        expect(spy).toHaveBeenCalledWith('dispatch', {
-          dispatchFunction:'receiveGamesByStatus', payload: {status:"pending", games: mockGames.slice(0,2)}
-        })
-        done()
-      },50)
+    setTimeout(() => {
+      expect(mockCreateGame.mock.calls[0][0]).toEqual(mockGames[0])
+      expect(spy).toHaveBeenCalledWith('dispatch', {
+        dispatchFunction: 'receiveGamesByStatus', payload: { status: "pending", games: mockGames.slice(0, 2) }
+      })
+      done()
+    }, 50)
+  })
+
+  test('socket calls db.updateGame then getGames, then emit dispatch to client socket', (done) => {
+    const id = 1
+    const name = "Harry's butt lol"
+    const password = "Harry is dumb lol"
+    const status = "playing"
+    const newGame = { id, name, password, status }
+
+    socket.emit(UPDATE_GAME, newGame)
+
+    setTimeout(()=> {
+      expect(mockUpdateGame.mock.calls[0][0]).toEqual(newGame)
+      expect(spy).toHaveBeenCalledWith('dispatch', {
+        dispatchFunction: 'receiveGamesByStatus', payload: { status: newGame.status, games: [mockGames[2]]}        
+      })
+      done()
+    } , 50)
+  })
+
+  test('socket calls db.deleteGame then getGames, then emit dispatch to client socket', (done) => {
+    const id = 1
+    socket.emit(DELETE_GAME, id)
+
+    setTimeout(() => {
+      expect(mockDeleteGame.mock.calls[0][0]).toEqual(id)
+      // expect(spy).toHaveBeenCalledWith('dispatch', {
+      //   dispatchFunction: 'receiveGamesByStatus', payload: { status: "pending", games: mockGames.slice(0, 2)}        
+      // })
+      done()
     })
+  })
 })
