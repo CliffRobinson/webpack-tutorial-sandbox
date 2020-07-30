faa228048133a22f48ea304e42de8db322a608f1
CliffRobinson <clifford.f.robinson@gmail.com>
2020-07-30 15:49:39 +1200

    herokery

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,6 @@
 const knex = require('knex');
-const config = require('../../knexfile').development//[process.env.NODE_ENV || 'development'];
+const environment = process.env.NODE_ENV || 'development'
+console.log(`Environment is ${environment}`);
+const config = require('../../knexfile')[/* process.env.NODE_ENV ||  */'development'];
 
 module.exports = knex(config);
